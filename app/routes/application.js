11f635498740102f2d86fa8eec8cfe41ab048c22
Isabelle Teixeira <isabelleroteixeira@gmail.com>
2021-11-13 23:48:15 +0000

    main page with athletes basic data

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,21 +1,12 @@
-import Route from '@ember/routing/route';
-import RSVP from 'rsvp';
+import Route from "@ember/routing/route";
+import RSVP from "rsvp";
 
 export default class ApplicationRoute extends Route {
-
   model() {
-    let athleteDocRef = 'PMgxRcGJrkEmrmDepgZM';
-    let athlete = this.store.findRecord('athlete', athleteDocRef);
-
-    // example query
-    let sleeps = this.store.query('sleep', {
-      query: ref => ref.where('athleteDocRef', '==', athleteDocRef)
-    });
+    let athletes = this.store.findAll("athlete");
 
     return RSVP.hash({
-      athlete,
-      sleeps
+      athletes,
     });
   }
-
 }
