6e134541b89cc1f0c4b4de757aa20a77d7dd1f50
Isabelle Teixeira <isabelleroteixeira@gmail.com>
2021-11-14 03:10:11 +0000

    created component athlete-card

--- app/models/athlete.js
+++ app/models/athlete.js
@@ -5,14 +5,32 @@ export default class AthleteModel extends Model {
   @attr("string") lastName;
   @attr("number") age;
   @attr("number") heightCm;
+  @attr("string") heightFeet;
   @attr("string") lastName;
   @attr("string") sport;
   @attr("number") weightKg;
-  @attr("number") photoUrl;
+  @attr("number") weightLbs;
+  @attr("string") photoUrl;
   @attr("string") tour;
-  @attr("number") team;
+  @attr("string") team;
 
   get fullName() {
     return `${this.firstName} ${this.lastName} `;
+  }
+
+  get height() {
+    return this.heightCm ? this.heightCm : this.heightFeet;
+  }
+
+  get heightMeasure() {
+    return this.heightCm ? "cm" : "feet";
+  }
+
+  get weight() {
+    return this.weightKg ? this.weightKg : this.weightLbs;
+  }
+
+  get weightMeasure() {
+    return this.weightKg ? "kg" : "lbs";
   }
 }
--- tests/unit/routes/athlete-test.js
+++ tests/unit/routes/athlete-test.js
@@ -1,11 +1,11 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
 
-module('Unit | Route | athlete', function(hooks) {
+module("Unit | Route | athlete", function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
-    let route = this.owner.lookup('route:athlete');
+  test("it exists", function (assert) {
+    let route = this.owner.lookup("route:athlete");
     assert.ok(route);
   });
 });
